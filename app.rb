e2e4c0eade8408894c53488e0973a811e791205d
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-6.local>
2017-01-20 14:01:39 +0000

    Changed gameplay

--- app.rb
+++ app.rb
@@ -15,8 +15,10 @@ post '/names' do
 end
 
 get '/play' do
-  @player_1_name = $game.current_attacker.name
-  @player_2_name = $game.current_victim.name
+  @player_1_name = $game.player_1.name
+  @player_2_name = $game.player_2.name
+  @attacker_name = $game.current_attacker.name
+  @victim_name = $game.current_victim.name
   @player_1_hp = $game.current_attacker.hp
   @player_2_hp = $game.current_victim.hp
   erb :play
@@ -25,8 +27,10 @@ end
 post '/play' do
   @attacked = true
   $game.attack($game.current_victim)
-  @player_1_name = $game.current_attacker.name
-  @player_2_name = $game.current_victim.name
+  @player_1_name = $game.player_1.name
+  @player_2_name = $game.player_2.name
+  @attacker_name = $game.current_attacker.name
+  @victim_name = $game.current_victim.name
   @player_1_hp = $game.current_attacker.hp
   @player_2_hp = $game.current_victim.hp
   $game.switch_players
